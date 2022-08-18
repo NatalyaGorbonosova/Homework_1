c739546505bd1a2ace1d8e82bbd8ec55c7df13dc
NatalyaGorbonosova <gorbonosova.n@mail.ru>
2022-08-18 21:15:34 +0300

    Task 1

---
+++ Example001_compare/Program.cs
@@ -0,0 +1,8 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Введите два целых числа: "); //Даем пользователю возможность ввести числа
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+if (a > b) Console.WriteLine(a + " больше " + b); //Сравниваем два числа
+else Console.WriteLine(a + " меньше " + b);
